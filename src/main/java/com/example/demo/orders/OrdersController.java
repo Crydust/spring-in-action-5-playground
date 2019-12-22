7217ecd96f9e2e551609ccae31bb3300a5616c12
Kristof Neirynck <k.neirynck@gmail.com>
2019-12-22 19:14:57 +0100

    Use repositories in OrdersController
    * retrieve order ModelAttribute from session (it was created earlier by the DesignTacoController)
    * remove temporary session variables by completing the session status

--- src/main/java/com/example/demo/orders/OrdersController.java
+++ src/main/java/com/example/demo/orders/OrdersController.java
@@ -2,34 +2,50 @@ package com.example.demo.orders;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrdersController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrdersController.class);
 
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrdersController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @GetMapping("/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+    public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping("")
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(
+            @Valid @ModelAttribute("order") Order order,
+            Errors errors,
+            SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
         LOGGER.info("Order submitted: {}", order);
+        orderRepository.save(order);
+        sessionStatus.setComplete();
+
         return "redirect:/";
     }
 }
