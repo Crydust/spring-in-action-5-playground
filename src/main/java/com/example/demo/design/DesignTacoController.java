a1f4bd6054474ff44a499fd796eae0dc391d62dd
Kristof Neirynck <k.neirynck@gmail.com>
2019-12-22 18:56:25 +0100

    Use repositories in DesignTacoController

--- src/main/java/com/example/demo/design/DesignTacoController.java
+++ src/main/java/com/example/demo/design/DesignTacoController.java
@@ -1,9 +1,9 @@
 package com.example.demo.design;
 
-import com.example.demo.Ingredient;
-import com.example.demo.Ingredient.Type;
+import com.example.demo.design.Ingredient.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 import static java.util.stream.Collectors.toList;
 
@@ -23,49 +24,53 @@ public class DesignTacoController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DesignTacoController.class);
 
-    private static final List<Ingredient> INGREDIENTS = List.of(
-            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Type.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-    );
+    private final IngredientRepository ingredientRepository;
+    private final TacoRepository tacoRepository;
 
-    private static List<Ingredient> ingredientsFilteredByType(List<Ingredient> ingredients, Type type) {
-        return ingredients.stream()
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository) {
+        this.ingredientRepository = ingredientRepository;
+        this.tacoRepository = tacoRepository;
+    }
+
+    private static List<Ingredient> ingredientsFilteredByType(Iterable<Ingredient> ingredients, Type type) {
+        return StreamSupport.stream(ingredients.spliterator(), false)
                 .filter(it -> it.getType() == type)
                 .collect(toList());
     }
 
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        model.addAttribute("wrap", ingredientsFilteredByType(INGREDIENTS, Ingredient.Type.WRAP));
-        model.addAttribute("protein", ingredientsFilteredByType(INGREDIENTS, Ingredient.Type.PROTEIN));
-        model.addAttribute("veggies", ingredientsFilteredByType(INGREDIENTS, Ingredient.Type.VEGGIES));
-        model.addAttribute("cheese", ingredientsFilteredByType(INGREDIENTS, Ingredient.Type.CHEESE));
-        model.addAttribute("sauce", ingredientsFilteredByType(INGREDIENTS, Ingredient.Type.SAUCE));
+        final Iterable<Ingredient> ingredients = ingredientRepository.findAll();
+        model.addAttribute("wrap", ingredientsFilteredByType(ingredients, Ingredient.Type.WRAP));
+        model.addAttribute("protein", ingredientsFilteredByType(ingredients, Ingredient.Type.PROTEIN));
+        model.addAttribute("veggies", ingredientsFilteredByType(ingredients, Ingredient.Type.VEGGIES));
+        model.addAttribute("cheese", ingredientsFilteredByType(ingredients, Ingredient.Type.CHEESE));
+        model.addAttribute("sauce", ingredientsFilteredByType(ingredients, Ingredient.Type.SAUCE));
+    }
+
+    @ModelAttribute("taco")
+    public Taco taco() {
+        return new Taco();
     }
 
     @GetMapping("")
-    public String design(Model model) {
-        model.addAttribute("design", new Taco());
-
+    public String designForm() {
         return "design";
     }
 
     @PostMapping("")
-    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+    public String processDesign(
+            @Valid @ModelAttribute("taco") Taco taco,
+             Errors errors) {
         if (errors.hasErrors()) {
-            LOGGER.error("Error processing design: {}", design);
+            LOGGER.error("Error processing design: {}", taco);
             return "design";
         }
 
-        LOGGER.info("Processing design: {}", design);
+        LOGGER.info("Processing design: {}", taco);
+        final Taco saved = tacoRepository.save(taco);
+        //order.addDesign(saved);
 
         return "redirect:/orders/current";
     }
