1450473e3c6597df825b8522461ead8a9af589c9
Kristof Neirynck <k.neirynck@gmail.com>
2019-12-25 20:41:34 +0100

    Add hypersistence-optimizer dependency

--- src/test/java/com/example/demo/DemoApplicationIT.java
+++ src/test/java/com/example/demo/DemoApplicationIT.java
@@ -1,10 +1,25 @@
 package com.example.demo;
 
+import io.hypersistence.optimizer.HypersistenceOptimizer;
+import io.hypersistence.optimizer.core.config.JpaConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
 @SpringBootTest
 public class DemoApplicationIT {
+    @PersistenceUnit
+    private EntityManagerFactory entityManagerFactory;
+
+    @BeforeEach
+    public void init() {
+        new HypersistenceOptimizer(
+                new JpaConfig(entityManagerFactory)
+        ).init();
+    }
 
     @Test
     public void contextLoads() {
