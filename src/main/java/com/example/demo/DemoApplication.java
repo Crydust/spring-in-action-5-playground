c09a873aecd0db5cc10928d781529b2841a1a2a3
Kristof Neirynck <k.neirynck@gmail.com>
2020-01-12 16:59:26 +0100

    Add login form

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -16,21 +22,54 @@ public class DemoApplication {
     }
 
     @Configuration
+    public static class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addViewController("/").setViewName("home");
+            registry.addViewController("/login").setViewName("login");
+        }
+
+    }
+
+    @Configuration
     @EnableWebSecurity
     public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        UserDetailsService userDetailsService;
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-            auth.inMemoryAuthentication()
-                    .passwordEncoder(passwordEncoder)
-                    .withUser("kn")
-                    .password(passwordEncoder.encode("k"))
-                    .roles("USER", "ADMIN")
-                    .and()
-                    .withUser("user")
-                    .password(passwordEncoder.encode("u"))
-                    .roles("USER");
+            auth
+                    .userDetailsService(userDetailsService)
+                    .passwordEncoder(passwordEncoder());
         }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/design", "/orders")
+                    .hasRole("USER")
+                    .antMatchers("/", "/*")
+                    .permitAll()
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/authenticate")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/design")
+                    .and()
+                    .logout()
+                    .logoutSuccessUrl("/");
+        }
+
     }
 
 }
